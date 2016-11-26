5c4b2e67d72e6d6ef2b9604a35893a31f2c901c5
henrywangx@gmail.com <xiowang@ShawnWang.local>
2016-11-26 20:52:29 +0800

    end this learning

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from rango.models import Page, Category
+from rango.models import Page, Category, User, UserProfile
 
 
 class CategoryForm(forms.ModelForm):
@@ -64,3 +64,17 @@ class PageForm(forms.ModelForm):
         exclude = ('category',)
         # or specify the fields to include (i.e. not include the category field)
         # fields = ('title', 'url', 'views')
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
