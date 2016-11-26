e73485cca0bd17575d9649bb798ad0376e06bf6a
xiowang <henrywangx@gmail.com>
2016-11-26 20:59:00 +0800

    update

---
+++ static/js/rango-jquery.js
@@ -0,0 +1,8 @@
+/**
+ * Created by xiowang on 26/11/2016.
+ */
+$(document).ready(function() {
+
+        // JQuery code to be added in here.
+
+        });
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,7 +82,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_PATH = os.path.join(BASE_DIR,'static')
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'  # You may find this is already defined as such.
 
@@ -101,3 +102,16 @@ TEMPLATE_DIRS = (
 # Media DIR
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Absolute path to the media directory
+# Hasher for password, default is django.contrib.auth.hashers.PBKDF2PasswordHasher
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+# login_url
+
+REGISTRATION_OPEN = True                # If True, users can register
+ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
+REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
+LOGIN_REDIRECT_URL = '/rango/'  # The page you want users to arrive at after they successful log in
+LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
+                                # and are trying to access pages requiring authentication
